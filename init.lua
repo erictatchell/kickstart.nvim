daa67f7283eb78d375afd5f0ab9b839609c5a329
erictatchell <erictatch@gmail.com>
2024-09-21 22:30:21 -0700

    removed java lsp

--- init.lua
+++ init.lua
@@ -474,7 +474,7 @@ require('lazy').setup({
         gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
-        java_language_server = {},
+        --java_language_server = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
